7b27fcc7ab4b67254f74a04b0383f38bc985dbe7
cnie <cnie@microstrategy.com>
2022-10-07 08:52:03 +0800

    Load image with coil

--- app/src/main/java/com/example/composelistdemo/MainScreen.kt
+++ app/src/main/java/com/example/composelistdemo/MainScreen.kt
@@ -1,29 +1,27 @@
 package com.example.composelistdemo
 
-import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun MainScreen() {
-    BoxWithConstraints {
-        Log.d("ncq", "minWidth = $minWidth maxWidth = $maxWidth")
-        Log.d("ncq", "minHeight = $minHeight maxHeight = $maxHeight")
-        Box(
+    Box(
+        modifier = Modifier
+            .height(100.dp)
+            .fillMaxWidth()
+            .padding(8.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        AsyncImage(
+            model = "https://picsum.photos/200",
+            contentDescription = null,
             modifier = Modifier
-                .height(100.dp)
-                .fillMaxWidth()
-                .background(Color.Blue)
-                .padding(8.dp),
-            contentAlignment = Alignment.Center,
-        ) {
-            Text(text = "Hello World!", color = Color.White)
-        }
+                .width(80.dp)
+                .height(80.dp),
+        )
     }
 }
